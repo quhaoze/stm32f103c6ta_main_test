77b8fa4e0d02b63834701799da7be80fdb405ac6
quhaoze <quhaoze110110@gmail.com>
2024-06-13 19:23:57 +0800

    6/13

---
+++ cmake-build-minsizerel/CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED_Data.c.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED_Data.c.obj: \
+ F:\dc1git\stm32f103c6ta_main_test\Core\Src\OLED_Data.c \
+ F:\dc1git\stm32f103c6ta_main_test\Core\Inc/OLED_Data.h \
+ f:\clion_to_keil\gcc\102021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\stdint.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\machine\_default_types.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\features.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\_newlib_version.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\_intsup.h \
+ f:\clion_to_keil\gcc\102021.10\arm-none-eabi\include\sys\_stdint.h
---
+++ cmake-build-minsizerel/CMakeFiles/STM32F103C6.elf.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Startup/startup_stm32f103c6tx.s" "F:/dc1git/stm32f103c6ta_main_test/cmake-build-minsizerel/CMakeFiles/STM32F103C6.elf.dir/Core/Startup/startup_stm32f103c6tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Inc"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/OLED.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/OLED_Data.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED_Data.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/OLED_Data.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/main.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/main.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/stm32f1xx_it.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/syscalls.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/syscalls.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/sysmem.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/sysmem.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Core/Src/system_stm32f1xx.c" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "F:/dc1git/stm32f103c6ta_main_test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/STM32F103C6.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
